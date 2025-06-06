ef94bcd8e8eaa035740e152c30b1160b056ed120
Dhruv Thummar <dvthummar258@gmail.com>
2025-06-06 12:54:43 +0530

    todo ready for deployment 4

--- scripts/pathError.js
+++
@@ -1,8 +0,0 @@
-
-export function pageNotFound() {
-    console.log(location.pathname)
-    if (location.pathname !== '/login.html' && location.pathname !== '/register.html' && location.pathname !== '/index.html') {
-        console.log('Page not found')
-        document.body.innerHTML = 'Page not found'
-    }
-}
